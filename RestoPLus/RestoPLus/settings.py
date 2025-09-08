16c9320e80279d02d0293c201a2a245de8444a86
Hasan Al-dulaimi <haald001@edu.cegepgarneau.ca>
2025-09-08 10:58:05 -0400

    Fix - ajouter les modules dans settings

--- RestoPLus/RestoPLus/settings.py
+++ RestoPLus/RestoPLus/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'comptes'
-    'employes'
+    'comptes',
+    'employes',
     'inventaires'
 ]
 
