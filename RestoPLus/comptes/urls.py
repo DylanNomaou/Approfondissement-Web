1330705c338c67390fa58c4e993b6266dce6a603
Olivier Lacroix <ollac004@edu.cegepgarneau.ca>
2025-09-17 10:25:10 -0400

    Supression du dossier inutile RestoPlus

--- RestoPLus/comptes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'comptes'
-
-urlpatterns = [
-    # Exemple de route pour l'application comptes
-    path('', views.index, name='index'),
-]
--- RestoPLus/inventaires/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    """Vue index pour l'application inventaires."""
-    return render(request, 'inventaires/index.html')
