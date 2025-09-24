129773d5af0fbcdeb731336120873a9e06aead62
Olivier Lacroix <ollac004@edu.cegepgarneau.ca>
2025-09-24 09:03:48 -0400

    Ajustement du RestoPlus en restoplus

--- ApprofondissementWeb/ApprofondissementWeb/settings.py
+++ ApprofondissementWeb/ApprofondissementWeb/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RestoPlus',
+    'restoplus',
 ]
 
 MIDDLEWARE = [
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'RestoPLus' / 'static',
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'restoplus' / 'static',
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
