804db6f35a20cff46556cdcbae3cb6639fe99e64
Dylannomaou <dylannomaou@gmail.com>
2025-09-23 23:05:03 -0400

    Ajout de css dans la page d'accueil ainsi qu'un menu de navigation sur le côté.

--- ApprofondissementWeb/RestoPlus/views.py
+++ ApprofondissementWeb/RestoPlus/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def accueil(request):
-    return render(request,"comptes/index.html")
+    """Vue pour la page d'accueil."""
+    return render(request,"accueil.html")
 #commentaire pour tester git
+
+
+@login_required
+def admin_dashboard(request):
+    """Vue pour le tableau de bord administrateur."""
+
+    context = {
+    'title': 'Tableau de bord administrateur',
+    }
+    return render(request, "resto/admin_dashboard.html", context)
