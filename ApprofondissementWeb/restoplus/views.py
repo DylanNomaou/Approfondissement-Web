3f2aa1a92c52311c07968afa50e9ad39a3c389ee
Olivier Lacroix <ollac004@edu.cegepgarneau.ca>
2025-09-24 10:16:43 -0400

    Ajustement de la structure pour l'ajout de base et l'accueil qui hérite de base. Ajustement du scss aussi dans le script pour qu'il soit pris en compte

--- ApprofondissementWeb/restoplus/views.py
+++ ApprofondissementWeb/restoplus/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def accueil(request):
     """Vue pour la page d'accueil."""
-    return render(request,"accueil.html")
+    return render(request,"restoplus/accueil.html")
 #commentaire pour tester git
 
 
