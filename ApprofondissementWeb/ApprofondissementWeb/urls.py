497738887ca5adecdae3a1a37957939544554607
olivier225 <ollac004@edu.cegepgarneau.ca>
2025-09-25 18:11:00 -0400

    ajustement du liens pour les styles

--- ApprofondissementWeb/ApprofondissementWeb/urls.py
+++ ApprofondissementWeb/ApprofondissementWeb/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from django.conf import settings
 from django.conf.urls.static import static
 from restoplus import views
 
@@ -23,3 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('restoplus.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
